96ebde9513f5b5f1d6417be80299e376d33b3668
Avalander <darth.quesorico@gmail.com>
2018-04-06 16:53:29 +0200

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+	name='PyEither',
+	version='0.1.0',
+	description='Implementation of the Either monad.',
+	url='https://github.com/Avalander/py-either',
+	author='Avalander',
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4'
+	],
+	keywords='either functional',
+	packages=find_packages(),
+	install_requires=[]
+)
